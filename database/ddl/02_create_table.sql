e7b2ba69ea1a519efd3ed8a3e84d04a73de0b844
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-10-15 01:17:26 +0900

    :wrench: 初期データの投入
    createではDB名をバッククォートで囲まないとエラーが出た

---
+++ database/ddl/02_create_table.sql
@@ -0,0 +1,15 @@
+USE echo-crud;
+
+CREATE TABLE IF NOT EXISTS pokemons(
+  id    INT           UNSIGNED NOT NULL AUTO_INCREMENT  COMMENT 'ID',
+  no    INT           UNSIGNED NOT NULL                 COMMENT 'No.',
+  name  VARCHAR(10)   NOT NULL                          COMMENT '名前',
+  species VARCHAR(10) NOT NULL                          COMMENT '分類',
+  h     INT           UNSIGNED NOT NULL                 COMMENT 'HP',
+  a     INT           UNSIGNED NOT NULL                 COMMENT 'こうげき',
+  b     INT           UNSIGNED NOT NULL                 COMMENT 'ぼうぎょ',
+  c     INT           UNSIGNED NOT NULL                 COMMENT 'とくこう',
+  d     INT           UNSIGNED NOT NULL                 COMMENT 'とくぼう',
+  s     INT           UNSIGNED NOT NULL                 COMMENT 'すばやさ',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ポケモン';
