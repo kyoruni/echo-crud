d4f710dcaf183b3f20b0bac0054be31493c3e855
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-11-25 17:02:33 +0900

    :wrench: 日付カラム追加 (#6)

--- database/ddl/03_insert_initial_data.sql
+++ database/ddl/03_insert_initial_data.sql
@@ -1,6 +1,6 @@
 USE echo-crud;
 
-INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s) VALUES (1, 1, 'フシギダネ', 'たね', 45, 49, 49, 65, 65, 45);
-INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s) VALUES (2, 4, 'ヒトカゲ', 'とかげ', 39, 52, 43, 60, 50, 65);
-INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s) VALUES (3, 7, 'ゼニガメ', 'かめのこ', 44, 48, 65, 50, 64, 43);
-INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s) VALUES (4, 25, 'ピカチュウ', 'ねずみ', 35, 55, 40, 50, 50, 90);
+INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s, created_on, modified_on) VALUES (1, 1, 'フシギダネ', 'たね', 45, 49, 49, 65, 65, 45, NOW(), NOW());
+INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s, created_on, modified_on) VALUES (2, 4, 'ヒトカゲ', 'とかげ', 39, 52, 43, 60, 50, 65, NOW(), NOW());
+INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s, created_on, modified_on) VALUES (3, 7, 'ゼニガメ', 'かめのこ', 44, 48, 65, 50, 64, 43, NOW(), NOW());
+INSERT INTO pokemons (id, no, name, species, h, a, b, c, d, s, created_on, modified_on) VALUES (4, 25, 'ピカチュウ', 'ねずみ', 35, 55, 40, 50, 50, 90, NOW(), NOW());
